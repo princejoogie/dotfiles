50a8ee51213b4900ae29fc1a73abedeb73f90c13
princejoogie <princejoogie@gmail.com>
2022-03-13 23:13:06 +0800

    fix hex color highlight

--- nvim/.config/nvim/lua/treesitter-config.lua
+++ nvim/.config/nvim/lua/treesitter-config.lua
@@ -3,6 +3,6 @@ require('nvim-treesitter.configs').setup {
   sync_install = true,
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = true,
   },
 }
