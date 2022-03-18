19b4c8d2528f9c6f5029493cc7e5c157004edb8e
princejoogie <princejoogie@gmail.com>
2022-03-18 15:15:58 +0800

    add lazygit toggle

--- nvim/.config/nvim/lua/term-config.lua
+++ nvim/.config/nvim/lua/term-config.lua
@@ -37,3 +37,20 @@ function _G.set_terminal_keymaps()
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({
+	cmd = "lazygit",
+	hidden = true,
+	direction = "float",
+	float_opts = {
+		border = "double",
+	},
+})
+
+function _Lazygit_toggle()
+	lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _Lazygit_toggle()<CR>", { noremap = true, silent = true })
+
