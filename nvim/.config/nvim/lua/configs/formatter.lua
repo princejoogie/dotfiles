24ab8e717b9a3b674be49b8118f65e10d94ef2e8
princejoogie <princejoogie@gmail.com>
2022-06-27 05:24:37 +0800

    remove extra setup

--- nvim/.config/nvim/lua/configs/formatter.lua
+++ nvim/.config/nvim/lua/configs/formatter.lua
@@ -1,19 +1,3 @@
-require("formatter").setup(
-  {
-    filetype = {
-      prisma = {
-        function()
-          return {
-            exe = "prisma-fmt",
-            args = {"format", "-i", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-            stdin = true
-          }
-        end
-      }
-    }
-  }
-)
-
 local prettier = {
   function()
     return {
