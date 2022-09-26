9f404d926cd43f4819ff533aec3762bfd796b6c9
princejoogie <princejoogie@gmail.com>
2022-09-26 21:10:29 +0800

    feat: multi monitor
    modified:   i3/.config/i3/config
    modified:   i3/.config/polybar/config
    modified:   i3/.config/polybar/launch.sh

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -6,6 +6,14 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1-joogie.log
 polybar joogie 2>&1 | tee -a /tmp/polybar-joogie.log & disown
