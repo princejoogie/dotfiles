10427a3a6c1f4244849af05181579639823149a3
princejoogie <princejoogie@gmail.com>
2022-09-26 22:06:08 +0800

    fix: xrandr multi screen
    modified:   i3/.config/i3/config
    new file:   i3/.config/i3/screen.sh
    modified:   i3/.config/picom/picom.conf
    modified:   i3/.config/polybar/launch.sh

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -6,16 +6,11 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
-  done
-else
-  polybar --reload example &
-fi
-
-# Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1-joogie.log
-polybar joogie 2>&1 | tee -a /tmp/polybar-joogie.log & disown
+
+MONITOR=$(xrandr --query | grep "primary" | cut -d" " -f1)
+polybar --reload joogie &
+
+# polybar joogie 2>&1 | tee -a /tmp/polybar-joogie.log & disown
 
 echo "Bars launched..."
