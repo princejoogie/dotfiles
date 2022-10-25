5b2c5e105c3a4679fc449fdf9bff9657d95a874c
princejoogie <princejoogie@gmail.com>
2022-10-26 00:00:45 +0800

    chore: format things
    modified:   nvim/.config/nvim/lua/lsp/settings/sumneko_lua.lua

--- nvim/.config/nvim/lua/lsp/settings/sumneko_lua.lua
+++ nvim/.config/nvim/lua/lsp/settings/sumneko_lua.lua
@@ -1,7 +1,7 @@
 local plugins_path = vim.fn.stdpath("data") .. "/site/pack/packer/start"
-
 local dir_list = vim.fn.glob(plugins_path .. "/*", true, true)
 local library_table = {}
+
 for _, v in ipairs(dir_list) do
 	library_table[v .. "/lua"] = true
 end
