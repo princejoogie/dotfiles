73995eb70f1713c09c7307f0dda4f285a95f588a
princejoogie <princejoogie@gmail.com>
2023-06-13 18:56:03 +0800

    fix: prettier format on weird nextjs file names AHAHA

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -9,7 +9,7 @@ return {
 			function()
 				return {
 					exe = "prettier",
-					args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)) },
+					args = { "--stdin-filepath", '"' .. vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)) .. '"' },
 					stdin = true,
 				}
 			end,
