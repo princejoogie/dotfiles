c166f521703b45b9e5be8eab4baaadec5ec565aa
princejoogie <princejoogie@gmail.com>
2023-11-13 06:30:48 +0800

    feat: add chatgpt plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -13,6 +13,18 @@ return {
 	"sindrets/diffview.nvim",
 	"Eandrju/cellular-automaton.nvim",
 	{
+		"jackMort/ChatGPT.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("chatgpt").setup()
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+	{
 		"David-Kunz/gen.nvim",
 		config = function()
 			require("gen").model = "mistral"
