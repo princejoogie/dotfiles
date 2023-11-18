28676728dc0dbf12a9763dfc612b7a969f59bf91
princejoogie <princejoogie@gmail.com>
2023-11-19 03:55:42 +0800

    feat: add mini animate

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -13,6 +13,38 @@ return {
 	"sindrets/diffview.nvim",
 	"Eandrju/cellular-automaton.nvim",
 	{
+		"echasnovski/mini.animate",
+		opts = function()
+			local mouse_scrolled = false
+			for _, scroll in ipairs({ "Up", "Down" }) do
+				local key = "<ScrollWheel" .. scroll .. ">"
+				vim.keymap.set({ "", "i" }, key, function()
+					mouse_scrolled = true
+					return key
+				end, { expr = true })
+			end
+
+			local animate = require("mini.animate")
+			return {
+				resize = {
+					timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+				},
+				scroll = {
+					timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+					subscroll = animate.gen_subscroll.equal({
+						predicate = function(total_scroll)
+							if mouse_scrolled then
+								mouse_scrolled = false
+								return false
+							end
+							return total_scroll > 1
+						end,
+					}),
+				},
+			}
+		end,
+	},
+	{
 		"jackMort/ChatGPT.nvim",
 		event = "VeryLazy",
 		config = function()
