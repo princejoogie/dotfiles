4f7e112910cb9eabac0c51365bb9c5fa7594fbca
princejoogie <princejoogie@gmail.com>
2024-03-22 00:58:45 +0800

    fix: nvim cmp config

--- nvim/.config/nvim/lua/joogie/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/joogie/plugins/autocomplete.lua
@@ -1,85 +1,149 @@
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-nvim-lua",
-		"hrsh7th/cmp-path",
-		"onsails/lspkind.nvim",
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-path",
+			"onsails/lspkind.nvim",
+		},
+		opts = function()
+			local cmp = require("cmp")
+			local lspkind = require("lspkind")
+
+			cmp.setup.filetype("gitcommit", {
+				sources = cmp.config.sources({
+					{ name = "cmp_git" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			cmp.setup.cmdline("/", {
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			cmp.setup.cmdline(":", {
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+
+			return {
+				mapping = {
+					["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+					["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+					["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+					["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+					["<C-x>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+					["<C-y>"] = cmp.config.disable,
+					["<C-e>"] = cmp.mapping({
+						i = cmp.mapping.abort(),
+						c = cmp.mapping.close(),
+					}),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				},
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
+					{ name = "path" },
+				}, {
+					{ name = "buffer" },
+					{ name = "spell" },
+				}),
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = lspkind.cmp_format({
+						mode = "symbol",
+						preset = "codicons",
+						maxwidth = 50,
+						before = function(entry, vim_item)
+							vim_item.menu = ({
+								nvim_lsp = "[LSP ]",
+								buffer = "[Buf ]",
+								path = "[Path]",
+								luasnip = "[Snip]",
+							})[entry.source.name]
+							return vim_item
+						end,
+					}),
+				},
+				window = {
+					completion = {
+						border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+					},
+					documentation = {
+						border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+					},
+				},
+			}
+		end,
+		config = function(_, opts)
+			for _, source in ipairs(opts.sources) do
+				source.group_index = source.group_index or 1
+			end
+			require("cmp").setup(opts)
+		end,
 	},
-	config = function()
-		local cmp = require("cmp")
-		local lspkind = require("lspkind")
-
-		cmp.setup({
-			mapping = {
-				["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-				["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-				["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-				["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-				["<C-x>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-				["<C-y>"] = cmp.config.disable,
-				["<C-e>"] = cmp.mapping({
-					i = cmp.mapping.abort(),
-					c = cmp.mapping.close(),
-				}),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			{
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
 			},
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "nvim_lua" },
-				{ name = "path" },
-			}, {
-				{ name = "buffer" },
-				{ name = "spell" },
-			}),
-			formatting = {
-				fields = { "kind", "abbr", "menu" },
-				format = lspkind.cmp_format({
-					mode = "symbol",
-					preset = "codicons",
-					maxwidth = 50,
-					before = function(entry, vim_item)
-						vim_item.menu = ({
-							nvim_lsp = "[LSP ]",
-							buffer = "[Buf ]",
-							path = "[Path]",
-						})[entry.source.name]
-						return vim_item
-					end,
-				}),
-			},
-			window = {
-				completion = {
-					border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+			{
+				"nvim-cmp",
+				dependencies = {
+					"saadparwaiz1/cmp_luasnip",
 				},
-				documentation = {
-					border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-				},
+				opts = function(_, opts)
+					opts.snippet = {
+						expand = function(args)
+							require("luasnip").lsp_expand(args.body)
+						end,
+					}
+					table.insert(opts.sources, { name = "luasnip" })
+				end,
 			},
-		})
-
-		cmp.setup.filetype("gitcommit", {
-			sources = cmp.config.sources({
-				{ name = "cmp_git" },
-			}, {
-				{ name = "buffer" },
-			}),
-		})
-
-		cmp.setup.cmdline("/", {
-			sources = {
-				{ name = "buffer" },
+		},
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
+		keys = {
+			{
+				"<tab>",
+				function()
+					return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+				end,
+				expr = true,
+				silent = true,
+				mode = "i",
 			},
-		})
-
-		cmp.setup.cmdline(":", {
-			sources = cmp.config.sources({
-				{ name = "path" },
-			}, {
-				{ name = "cmdline" },
-			}),
-		})
-	end,
+			{
+				"<tab>",
+				function()
+					require("luasnip").jump(1)
+				end,
+				mode = "s",
+			},
+			{
+				"<s-tab>",
+				function()
+					require("luasnip").jump(-1)
+				end,
+				mode = { "i", "s" },
+			},
+		},
+	},
 }
