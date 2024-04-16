54e957951613ca4d3158cb1300759e86ecfa6721
princejoogie <princejoogie@gmail.com>
2024-04-16 12:14:43 +0800

    fix: vrc file format to json

--- nvim/.config/nvim/lua/joogie/plugins/util.lua
+++ nvim/.config/nvim/lua/joogie/plugins/util.lua
@@ -57,7 +57,7 @@ return {
 		"diepm/vim-rest-console",
 		config = function()
 			vim.g.vrc_response_default_content_type = "application/json"
-			vim.g.vrc_output_buffer_name = "__VRC_OUTPUT__"
+			vim.g.vrc_output_buffer_name = "__VRC_OUTPUT.json"
 			vim.g.vrc_auto_format_response_patterns = {
 				json = "jq",
 			}
