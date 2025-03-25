db308f08a35add43c59af4b48533d49db00e8e2b
princejoogie <princejoogie@gmail.com>
2025-03-25 13:26:16 +0800

    chore: add notifs on aider actions

--- nvim/.config/nvim/lua/joogie/plugins/aider.lua
+++ nvim/.config/nvim/lua/joogie/plugins/aider.lua
@@ -1,3 +1,8 @@
+local cmd_notif = function(cmd)
+  Snacks.notifier(cmd)
+  vim.cmd(cmd)
+end
+
 return {
   {
     "GeorgesAlkhouri/nvim-aider",
@@ -20,14 +25,16 @@ return {
         },
       })
     end,
+    -- stylua: ignore
     keys = {
+      { "<leader>ai", "<C-\\><C-n><cmd>AiderTerminalToggle<cr>", desc = "Open Aider", mode = { "t" } },
       { "<leader>ai", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
       { "<leader>as", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
       { "<leader>ac", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
       { "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
-      { "<leader>a=", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
-      { "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
-      { "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
+      { "<leader>a=", function() cmd_notif("AiderQuickAddFile") end, desc = "Add File to Aider" },
+      { "<leader>a-", function() cmd_notif("AiderQuickDropFile") end, desc = "Drop File from Aider" },
+      { "<leader>ar", function() cmd_notif("AiderQuickReadOnlyFile") end, desc = "Add File as Read-Only" },
     },
   },
 }
