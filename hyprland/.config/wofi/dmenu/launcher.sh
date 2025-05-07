acb0dab1f2f7b4246ec39e675f3b6d0486f1aca9
princejoogie <princejoogie@gmail.com>
2025-05-07 17:41:21 +0800

    chore: add dmenu launcher

---
+++ hyprland/.config/wofi/dmenu/launcher.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR="$HOME/dotfiles/hyprland/.config/wofi/dmenu/scripts"
+SCRIPTS=$(ls "$SCRIPT_DIR")
+
+selected=$(printf "%s\n" "$SCRIPTS" | wofi --dmenu -p "Run script")
+
+# Exit if user presses Escape (no input at all)
+[ -z "$selected" ] && exit
+
+if printf "%s\n" "$SCRIPTS" | grep -Fxq "$selected"; then
+  exec "$SCRIPT_DIR/$selected"
+else
+  printf "%s" "$selected" | jq -sRr @uri | xargs -r -I{} xdg-open "https://www.google.com/search?q={}"
+fi
