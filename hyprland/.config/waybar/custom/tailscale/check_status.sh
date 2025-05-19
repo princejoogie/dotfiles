2de24cee7e96c8e37f3be75100794dff7aa8a3de
princejoogie <princejoogie@gmail.com>
2025-05-19 18:06:13 +0800

    chore: show tailscale tooltip

--- hyprland/.config/waybar/custom/tailscale/check_status.sh
+++ hyprland/.config/waybar/custom/tailscale/check_status.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-STATE=$(tailscale status --json | jq -r '.BackendState')
+status_json=$(tailscale status --json)
+state=$(jq -r '.BackendState' <<< "$status_json")
+hostname=$(jq -r '.Self.HostName' <<< "$status_json")
+tailscale_ips=$(jq -r '.Self.TailscaleIPs[]' <<< "$status_json" | sed 's/^/ - /' | paste -sd '\r' -)
+tailnet=$(jq -r '.CurrentTailnet.Name' <<< "$status_json")
+online=$(jq -r '.Self.Online' <<< "$status_json")
+peer_count=$(jq '.Peer | length' <<< "$status_json")
+peers=$(jq -r '.Peer | to_entries[] | " - \((if .value.Online then "󱘖 " else " " end) + .value.DNSName)"' <<< "$status_json" | paste -sd '\r' -)
+[[ -z "$peers" ]] && peers="  - No peers found"
 
-if [[ "$STATE" == "Running" ]]; then
-  echo '{"text": "ON", "class": "running"}'
+tooltip="Tailnet: $tailnet\rDevice: $hostname\rStatus: $( [[ $online == true ]] && echo "Online" || echo "Offline" )\rTailscale IPs:\r$tailscale_ips\rPeers ($peer_count):\r$peers"
+
+if [[ "$state" == "Running" ]]; then
+  echo "{\"text\": \"ON\", \"tooltip\": \"$tooltip\"}"
 else
-  echo '{"text": "OFF", "class": "stopped"}'
+  echo "{\"text\": \"OFF\", \"tooltip\": \"VPN is not running\"}"
 fi
