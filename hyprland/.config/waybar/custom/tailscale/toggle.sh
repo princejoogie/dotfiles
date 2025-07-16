ce0895c4e8cd0fd002b127a95bf6cdd16d87c16b
princejoogie <princejoogie@gmail.com>
2025-07-16 21:08:53 +0800

    feat: add mapping for float tms

--- hyprland/.config/waybar/custom/tailscale/toggle.sh
+++ hyprland/.config/waybar/custom/tailscale/toggle.sh
@@ -5,5 +5,5 @@ STATE=$(tailscale status --json | jq -r '.BackendState')
 if [[ "$STATE" == "Running" ]]; then
   tailscale down
 else
-  tailscale up --accept-routes
+  tailscale up --accept-routes --advertise-exit-node --advertise-routes=192.168.1.0/24
 fi
