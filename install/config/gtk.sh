b0c22fe5ea1e1ec599616f847dc68fe479a51412
princejoogie <princejoogie@gmail.com>
2026-02-11 05:05:50 +0800

    fix: symlink GTK4 theme for libadwaita apps (Nautilus, etc.)

--- install/config/gtk.sh
+++ install/config/gtk.sh
@@ -5,4 +5,13 @@ if command -v gsettings &>/dev/null; then
   gsettings set org.gnome.desktop.interface icon-theme "Adwaita" 2>/dev/null || true
 fi
 
+# Link GTK4/libadwaita theme (Nautilus, GNOME apps ignore gtk-theme gsetting)
+THEME_DIR="/usr/share/themes/catppuccin-mocha-blue-standard+default/gtk-4.0"
+if [[ -d "$THEME_DIR" ]]; then
+  mkdir -p ~/.config/gtk-4.0
+  ln -snf "$THEME_DIR/assets" ~/.config/gtk-4.0/assets
+  ln -snf "$THEME_DIR/gtk.css" ~/.config/gtk-4.0/gtk.css
+  ln -snf "$THEME_DIR/gtk-dark.css" ~/.config/gtk-4.0/gtk-dark.css
+fi
+
 echo "GTK theme: OK"
