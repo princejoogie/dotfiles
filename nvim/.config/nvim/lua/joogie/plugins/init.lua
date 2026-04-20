82579fdda8eab17255d51d99b6ba137469401892
princejoogie <princejoogie@gmail.com>
2026-04-20 11:30:17 +0800

    chore: add codex

--- nvim/.config/nvim/lua/joogie/plugins/init.lua
+++ nvim/.config/nvim/lua/joogie/plugins/init.lua
@@ -272,5 +272,11 @@ return {
     opts = {
       lang = "typescript",
     },
+  },
+  {
+    'chomosuke/typst-preview.nvim',
+    lazy = false, -- or ft = 'typst'
+    version = '1.*',
+    opts = {}, -- lazy.nvim will implicitly calls `setup {}`
   }
 }
